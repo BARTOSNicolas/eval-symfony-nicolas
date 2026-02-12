95a88f8fbdf38050694bfdfe27590b6fec4ff948
BARTOSNicolas <73882719+BARTOSNicolas@users.noreply.github.com>
2026-02-12 14:58:44 +0100

    feat: ajout du Service Task

---
+++ evalsymfony/src/Service/TaskService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Task;
+
+class TaskService {
+    public function __construct(
+        private readonly TaskRepository $taskRepository,
+        private readonly EntityManagerInterface $em
+    ) {}
+
+    public function addTask(Task $task): Task {
+        try {
+            // si pas de tâche
+            if (!$task) {
+                throw new \Exception('La tâche n\'existe pas' );
+            }
+            // Ajout en BDD
+            $this->em->persist($task);
+            $this->em->flush();
+
+            // Return la tâche
+            return $task;
+
+        } catch (\Exception $error) {
+            throw new \Exception('Une erreur est surevnue pendant l\'ajout de la tâche' . $error);
+        }
+    }
+
+    public function getAllTask(): array {
+        try {
+            return $this->taskRepository->findAll();
+        } catch (\Exception $error) {
+            throw new \Exception('Erreur quand on récupère les tâches : ' . $error);
+        }
+    }
+}
